2563af169724042b968b41b8e3372b33d120c31d
Ricardo Salinas <ricardo_salinas91@hotmail.com>
2018-11-07 12:47:44 -0600

    Update Arrays/01-firstDuplicate.cs
    Update solution

--- Arrays/01-firstDuplicate.cs
+++ Arrays/01-firstDuplicate.cs
@@ -1,12 +1,12 @@
 int firstDuplicate(int[] a) {
+    char[] b = new char[a.Length];
     int i;
     for (i = 0; i < a.Length; i++) 
     {
-        if(a[Math.Abs(a[i]) - 1] < 0)
-            return Math.Abs(a[i]); 
-            
-        a[Math.Abs(a[i]) - 1] = -a[Math.Abs(a[i]) - 1];
+        if(b[a[i] - 1] == 'd'){
+            return a[i];
+        }
+        b[a[i] - 1] = 'd';
     }
     return -1;
 }
-
